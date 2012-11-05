a9c6e48b6e71ee706466195f68eefb7a79a3068c
Brett Slatkin <bslatkin@gmail.com>
2012-11-04 22:30:39 -0800

    slowly working through d3 stacked graphs

--- cohorts.js
+++ cohorts.js
@@ -97,11 +97,6 @@ function createGroupValueCheckboxes(groupTypes) {
 }
 
 
-function updateViz(data, groupTypes) {
-  
-};
-
-
 function getCohort(cohortDay) {
   // TODO: Do date-based grouping with start/end times.
   return cohortDay;
@@ -115,9 +110,16 @@ function filterData(rows, groupType, groupValues) {
 
   // Filter matching group type and group values.
   $.each(rows, function(index, value) {
+    // Skip the header row.
+    if (index == 0) {
+      return;
+    }
+    // Skip rows of the wrong group type.
     if (groupType !== value[GROUP_TYPE_COLUMN]) {
       return;
     }
+    // Skip rows with unmatched group values. Match everything if no values
+    // were supplied.
     if (groupValues.length > 0 &&
         groupValues.indexOf(value[GROUP_VALUE_COLUMN]) == -1) {
       return;
@@ -143,12 +145,42 @@ function filterData(rows, groupType, groupValues) {
   var keys = $.map(cohorts, function(value, key) { return key; });
   keys.sort();
 
-  // Return an array of arrays, with the cohort as the first column.
-  var result = [];
-  $.each(keys, function(index, value) {
-    result.push([value].concat(cohorts[value]))
+  // Now regroup the data as sets of points grouped by column (for D3).
+  var columns = {};
+  var headers = rows[0].slice(3);
+  $.each(cohorts, function(key, value) {
+    $.each(headers, function(index, header) {
+      var data = columns[header];
+      if (!data) {
+        data = []
+        columns[header] = data;
+      }
+      data.push({cohort: key, height: value[index]});
+    });
   });
-  return result;
+
+  var data = [];
+  $.each(columns, function(key, value) {
+    var total = 0;
+    $.each(value, function(index, cell) {
+      total += cell.height;
+    });
+    data.push({name: key, values: value, total: total});
+  });
+
+  return [keys, data];
+
+  // 
+  // // Sort cohort keys in ascending order.
+  // var keys = $.map(cohorts, function(value, key) { return key; });
+  // keys.sort();
+  // 
+  // // Return an array of arrays, with the cohort as the first column.
+  // var result = [];
+  // $.each(keys, function(index, value) {
+  //   result.push([value].concat(cohorts[value]))
+  // });
+  // return result;
 }
 
 
@@ -156,7 +188,8 @@ function extractGroupTypes(csvData) {
   // Maps cohort group types to lists of cohort group values
   var cohortGroupTypes = {};
 
-  for (var i = 0; i < csvData.length; ++i) {
+  // Skip the header row
+  for (var i = 1; i < csvData.length; ++i) {
     var row = csvData[i];
     var groupType = row[GROUP_TYPE_COLUMN];
     var groupValue = row[GROUP_VALUE_COLUMN];
@@ -178,17 +211,80 @@ function updateViz(rows) {
   var groupType = getSelectedGroupType();
   var groupValues = getSelectedGroupValues();
   var view = filterData(rows, groupType, groupValues);
+  var viewCohorts = view[0];
+  var viewBarGroups = view[1];
   console.log('Filtered to: type="' + groupType +
               '", values="' + (groupValues.join('|')) +
-              '"; ' + view.length + ' rows found');
+              '"; ' + viewCohorts.length + ' rows found');
+  console.log(viewBarGroups);
+
+  var height = 400;
+  var width = 500;
+
+  var scaleX = d3.scale.linear()
+      .range([0, width])
+      .domain([0, viewCohorts.length]);
+  // TODO: Have a scale object for each column
+  var scaleY = d3.scale.linear()
+      .range([0, height])
+      .domain([0, d3.max(viewBarGroups, function(d) { return d.total; })]);
+  var scaleColor = d3.scale.category20();
+
+  var getColor = function(d) {
+    return scaleColor(viewCohorts.indexOf(d.cohort));
+  };
+  var getX = function(d) {
+    return scaleX(viewCohorts.indexOf(d.cohort));
+  };
+  var getY = function(d) {
+    return -scaleY(d.height);
+  };
+  var getHeight = function(d) {
+    return scaleY(d.height);
+  };
+
+  var chart = d3.select('#viz_graph1').select('svg.stacked')
+        .attr('width', width)
+        .attr('height', height)
+      .append('svg:g')
+        .attr('transform', 'translate(0, 400)');
+
+  var stack = d3.layout.stack()
+    .values(function(d) { return d.values; })(viewBarGroups);
+
+  var layers = chart.selectAll('g.layer')
+      .data(stack)
+    .enter().append('svg:g')
+      .attr('class', 'layer')
+      .style('fill', getColor)
+      .style('stroke', 0);
+
+  var bars = layers.selectAll('g.bar')
+      .data(function(d) { return d.values; })
+    .enter().append('svg:rect')
+      .attr('class', 'bar')
+      .attr('width', 30)
+      .attr('x', getX)
+      .attr('y', getY)
+      .attr('height', getHeight);
+
+  // chart.selectAll('rect').data(stack(viewBarGroups));
+
+      // 
+      // .data([10, 20, 30])
+      // .enter()
+      //   .append('svg:rect')
+      //     .attr('x', 0)
+      //     .attr('y', function(d, i) { return 10 * i + 1; })
+      //     .attr('height', 25)
+      //     .attr('width', 10);
 }
 
 
 function handleClickVisualize() {
   // Parse the CSV data
-  var rowWithHeader = $.csv.toArrays($('#data').val());
-  var rows = rowWithHeader.slice(1)
-  var groupTypes = extractGroupTypes(rows);
+  var rowsWithHeader = $.csv.toArrays($('#data').val());
+  var groupTypes = extractGroupTypes(rowsWithHeader);
 
   // Setup UI
   createGroupTypeRadios(groupTypes);
@@ -197,7 +293,7 @@ function handleClickVisualize() {
   // Register event handlers
   $(document).unbind('cohorts.viz');
   $(document).bind('cohorts.viz', function() {
-    updateViz(rows);
+    updateViz(rowsWithHeader);
   });
 
   // Build chart
